0a7afca4c7e0ff9a1f8d99b9c4dd23071c073f68
DuurenbayarB <duureedue@gmail.com>
2026-05-24 07:28:00 +0000

    daalgavar6

---
+++ test.py
@@ -0,0 +1,11 @@
+a = int(input("a taliin urtiig oruul"))
+b = int(input("b taliin urtiig oruul"))
+c = int(input("c taliin urtiig oruul"))
+if a==b==c:
+    print("ene gurvaljin zov gurvaljin bain")
+elif a==b<c:
+    print("ene gurvaljin adil hajuut gurvaljin bain")
+elif a<b==c:
+    print("ene gurvaljin adil hajuut gurvaljin bain")
+else :
+    print("ene gurvaljin eldev talt gurvaljin bain")
